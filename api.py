26db96b78d979f8a35ccceec8ad38a408c5448d6
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-06-09 15:38:21 +0300

    Created the class for the products in db.

--- api.py
+++ api.py
@@ -9,6 +9,23 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+# Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
 @app.route("/", methods=['GET'])
 def getFunction():
     return jsonify( [
