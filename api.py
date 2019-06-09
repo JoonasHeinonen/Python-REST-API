dcd654447bd05d60380e372b2cd26c53a7deb213
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-06-09 15:33:04 +0300

    First actual commit.

--- api.py
+++ api.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+@app.route("/", methods=['GET'])
+def getFunction():
+    return jsonify( [
+                        {
+                            'message': 'HTTP method \'GET\' was called!'
+                        }
+                    ]
+                )
 
 if __name__ == '__main__':
     app.run(debug=True)
