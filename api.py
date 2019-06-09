2d1df371987edc1893ec29d0129d85d73b64aef8
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-06-09 16:26:06 +0300

    Added all HTTP methods needed.

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -26,14 +26,79 @@ class Product(db.Model):
         self.price = price
         self.quantity = quantity
 
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'quantity')
+
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
+# The method to create a Product using HTTP method 'POST'
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    new_product = Product(name, description, price, quantity)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+# The method to get all products, using HTTP method 'GET'
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result.data)
+
+# A method to get a single product using GET method
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+# The method to update an existing product, PUT method from HTTP is used
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.quantity = quantity
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
 @app.route("/", methods=['GET'])
 def getFunction():
-    return jsonify( [
-                        {
-                            'message': 'HTTP method \'GET\' was called!'
-                        }
-                    ]
-                )
+    return jsonify( 
+        [
+            {
+                'message': 'HTTP method \'GET\' was called!'
+            }
+        ]
+    )
+
+# Delete product using method DELETE
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    
+    return product_schema.jsonify(product)
 
 if __name__ == '__main__':
     app.run(debug=True)
